7998b6b475621eb92c5bd925dc18324884774c85
tolgaucar <tolgalgg@gmail.com>
2023-02-22 00:52:47 +0300

    installing express-handlebars and template engine integration

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
@@ -6,9 +7,28 @@ const hostname = 'localhost';
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'views/index.html'));
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('frontend/index');
 });
+
+app.get('/hakkimizda', (req,res) =>{
+    res.render('frontend/hakkimizda');
+})
+
+app.get('/filmler', (req,res) =>{
+    res.render('frontend/filmler');
+})
+
+app.get('/iletisim', (req,res) =>{
+    res.render('frontend/iletisim');
+})
+
+
+
 
 app.listen(port, hostname, (req, res) => {
     console.log('listening on port  ' + port); 
